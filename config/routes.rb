7e4b3fd82ca2a07adc1d91c6bad7c3083ae01342
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-05-24 15:50:51 +0100

    added name spacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
+
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
 end

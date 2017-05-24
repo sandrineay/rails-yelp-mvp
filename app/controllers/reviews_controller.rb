cd8d763ad991ab93f7ee6a94c77888a37e5da73a
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-05-24 15:31:35 +0100

    fixed all model validation and redirections green rake need to add admin

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
   def new
     # we need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
     # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
